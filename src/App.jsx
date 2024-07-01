1bd8918bebd1a8f323bcfa5f89dda00d86e55e12
Ashrafpallat <ashrafpallat123@gmail.com>
2024-07-01 10:35:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ComponentA from "./ComponetA"
+function App(){
+  return(
+    <div>
+      <ComponentA/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/ComponetC.jsx
@@ -0,0 +1,10 @@
+import ComponentD from "./ComponetD"
+function ComponentC(){
+    return(
+        <div className="box">
+            <h1>Component C</h1>
+            <ComponentD />
+        </div>
+    )
+}
+export default ComponentC
